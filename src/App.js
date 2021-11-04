e1a9893cc3d840bd97fcefd16a8eb3856f02e728
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-11-04 09:29:56 -0500

    added functions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,27 @@ const App = () => {
 
   const [product, setProduct] = useState([])
 
+
+  const handlenewNameChange = (event)=>{
+   setName(event.target.value);
+  }
+
+  const handleNewDescriptionChange = (event) => {
+   setDes(event.target.value);
+  }
+
+  const handleNewImageChange = (event) => {
+    setImage(event.target.value);
+  }
+
+  const handleNewQuantityChange = (event) => {
+    setQty(event.target.value);
+  }
+
+  const handleNewPriceChange = (event) => {
+    setPrice(event.target.value);
+  }
+
   useEffect(() => {
     axios
       .get('https://safe-oasis-61254.herokuapp.com/store')
