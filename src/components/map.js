80a7eb11ac2dd3e151e10ab93cf757402ddddbe6
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-11-05 16:15:26 -0500

    added front login

--- src/components/map.js
+++ src/components/map.js
@@ -8,16 +8,18 @@ const Map = (props) => {
           return (
             <div>
               <img className="prodimg" src={products.image}/>
-              <h2 className="interesting">{products.name}</h2>
-              <h3>{products.description}</h3>
-              <h2>{products.price}</h2>
-              <h2>{products.qty}</h2>
+              <div className="underpic">
+                <h2 className="prodName">{products.name}</h2>
+                <h3>{products.description}</h3>
+                <h2>{products.price}</h2>
+                <h2>{products.qty}</h2>
 
-              <button onClick={ (event) => {props.handleDelete(products)}}>Bought!</button>
+                <button onClick={ (event) => {props.handleDelete(products)}}>Bought!</button>
 
-              <a href="#open-edit-modal">
-              <button onClick={ (event) => {props.updateButton(products)}}>Edit</button>
-              </a>
+                <a href="#open-edit-modal">
+                <button onClick={ (event) => {props.updateButton(products)}}>Edit</button>
+                </a>
+              </div>
             </div>
           )
         })
