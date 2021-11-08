82e0c03226f07c8e6f4c3d70060b9a4ce17a77f3
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-11-08 15:21:36 -0600

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ const App = () => {
   const [search, setSearch] = useState('')
   const [filtered, setFiltered] = useState([])
 
+  const [newQty, setNewQty] = useState('')
+
 ////////////////////////////////////////////////////////
 ////////////////AUTHENTICATION SECTION//////////////////
   //Authentication State Manipulation
@@ -138,6 +140,7 @@ const App = () => {
   const handleNewQuantityChange = (event) => {
     setQty(event.target.value)
     setEditQty(event.target.value)
+    setNewQty(event.target.value)
   }
 
   const handleNewPriceChange = (event) => {
@@ -145,8 +148,8 @@ const App = () => {
     setEditPrice(event.target.value)
   }
 
-  const handleSellerName = (event) => {
-    setSellerName(event.target.value)
+  const handleSellerName = () => {
+    setSellerName(currentUser.username)
   }
 
   const handleNewProductFormSubmit = (event) => {
@@ -165,7 +168,7 @@ const App = () => {
       axios
           .get('https://safe-oasis-61254.herokuapp.com/store')
           .then((response) => {
-              setProducts(response.data)
+            setProducts(response.data)
         })
     })
   }
@@ -188,7 +191,25 @@ const App = () => {
           .get('https://safe-oasis-61254.herokuapp.com/store')
           .then((response) => {
             setProducts(response.data)
-          })
+      })
+    })
+  }
+
+  const handleBuyQty = (e) => {
+    e.preventDefault()
+    axios
+    .put(
+      `https://safe-oasis-61254.herokuapp.com/store/${id}`,
+        {
+          qty:newQty
+        }
+    )
+    .then(()=> {
+      axios
+          .get('https://safe-oasis-61254.herokuapp.com/store')
+          .then((response) => {
+            setProducts(response.data)
+      })
     })
   }
 
@@ -199,6 +220,10 @@ const App = () => {
     setEditPrice(productData.price)
     setEditQty(productData.qty)
     setId(productData._id)
+  }
+
+  const buyButton = (productData) => {
+    setNewQty(productData.qty - 1)
   }
 
   const handleDelete = (productData)=> {
@@ -236,73 +261,66 @@ const App = () => {
 
   return (
     <main>
-    <header>
-      <div className="header-container">
-        <h2 className="header-words">
-          <span id="left">Quick Cash</span>
-          {currentUser.username ?
-          <div id="sell-link">
-            <span id='right'>
-              <a class="list-button" href="#open-create-modal">Sell</a>
-            </span>
-          </div>
-          : null}
-        </h2>
-      </div>
-      <div className="App">
-       <div>
-         {toggleLogout ?
-           <button onClick={handleLogout} class='logoutBtn'>Logout</button> :
-           <div class='appFormDiv'>
-             {toggleLogin ?
-               //login form
-               <div className="formContainer">
-                 <h1 class='formTitle'>Login</h1>
-                 <form onSubmit={handleLogin} class='inputForm'>
-                   <input type='text' placeholder='username' class='textInput' onChange={(event)=> {setUsername(event.target.value)}}/>
-                   <input type='password' placeholder='password' class='textInput' onChange={(event)=> {setPassword(event.target.value)}}/>
-                   {toggleError ?
-                     <h5 class='errorMsg'>{errorMessage}</h5>
-                     :
-                     null
-                   }
-                   <input type='submit' value='Login' class='submitBtn'/>
-                 </form>
-               </div>
-             :
-             // new user form
-             <div className="App" class='formContainer'>
-               <h1 class='formTitle'>Create an Account</h1>
-               <form onSubmit={handleCreateUser} class='inputForm'>
-                 <input type='text' placeholder='username' class='textInput' onChange={(event)=> {setUsername(event.target.value)}}/>
-                 <input type='password' placeholder='password' class='textInput' onChange={(event)=> {setPassword(event.target.value)}}/>
-                 {toggleError ?
+
+      <header>
+        <div className="header-container">
+          <h2 className="header-words">
+            <span id="left">Quick Cash</span>
+            {currentUser.username ?
+              <div>
+                <div class='loggedInDiv'>
+                  <h4>
+                    This entire div will only show if a user is currently logged in
+                  </h4>
+                </div>
+                <div id="sell-link">
+                  <span className='right'>
+                    <a class="list-button" href="#open-create-modal">Sell</a>
+                  </span>
+                </div>
+                <div>
+                  {toggleLogout ?
+                    <button onClick={handleLogout} class='logoutBtn'>Logout</button>
+                    :null
+                  }
+                </div>
+              </div>
+            :
+            <div className="App">
+              {toggleLogin ?
+              //login form
+                <div className="formContainer">
+                  <form onSubmit={handleLogin} class='inputForm'>
+                    <input type='text' placeholder='username' class='textInput' onChange={(event)=> {setUsername(event.target.value)}}/>
+                    <input type='password' placeholder='password' class='textInput' onChange={(event)=> {setPassword(event.target.value)}}/>
+                    {toggleError ?
+                      <h5 class='errorMsg'>{errorMessage}</h5>
+                      :null
+                    }
+                     <input type='submit' value='Login' class='submitBtn'/>
+                  </form>
+                </div>
+              :
+              // new user form
+                <div className="App" class='formContainer'>
+                <form onSubmit={handleCreateUser} class='inputForm'>
+                  <input type='text' placeholder='username' class='textInput' onChange={(event)=> {setUsername(event.target.value)}}/>
+                  <input type='password' placeholder='password' class='textInput' onChange={(event)=> {setPassword(event.target.value)}}/>
+                  {toggleError ?
                    <h5 class='errorMsg'>{errorMessage}</h5>
-                   :
-                   null
+                   :null
                  }
-                 <input type='submit' value='Register' class='submitBtn'/>
-               </form>
-             </div>
-             }
-             <button onClick={handleToggleForm} class='accountBtn'>{toggleLogin ? 'Need an account?' : 'Already have an account?'}</button>
-           </div>
-         }
+                  <input type='submit' value='Register' class='submitBtn'/>
+                </form>
+                </div>
+              }
+              <button onClick={handleToggleForm} class='accountBtn'>{toggleLogin ? 'Need an account?' : 'Already have an account?'}</button>
+            </div>
+            }
+          </h2>
+        </div>
+      </header>
 
-
-       </div>
-       {currentUser.username ?
-         <div class='loggedInDiv'>
-           <h1>This entire div will only show if a user is currently logged in</h1>
-           <h2>So you could show profile info, or whatever else you want to be authentication protected!</h2>
-           <h3>And you could even stick other React components in here!</h3>
-         </div>
-         :
-         null
-       }
-     </div>
-
-    </header>
       <div id="open-create-modal" className="modal">
         <div className="modal-text">
           <h2>List a product to sell</h2>
@@ -312,8 +330,9 @@ const App = () => {
             Description: <input type="text" onChange={handleNewDescriptionChange} /><br/>
             Price: <input type="number" min='0' onChange={handleNewPriceChange} /><br/>
             Quantity: <input type="number" min='0' onChange={handleNewQuantityChange} /><br/>
-            Seller: <input type="text"  onChange={handleSellerName}/><br/>
-            <input type="submit" value="create product" />
+            Seller: <input type="text" value={null} autocomplete='off'
+            onChange={handleSellerName}/><br/>
+            <input type="submit" value="create product"/>
           </form>
           <a href="#" class="close-modal">Close</a>
         </div>
@@ -322,34 +341,32 @@ const App = () => {
       <div id="open-edit-modal" className="modal">
         <div className="modal-text">
           <form onSubmit={handleUpdatesToProduct}>
-              Name: <input type="text" onChange={handlenewNameChange} value={editName}/><br/>
-              Image: <input type="text" onChange={handleNewImageChange} value={editImage}/><br/>
-              Description: <input type="text" onChange={handleNewDescriptionChange} value={editDescription}/><br/>
-              Price: <input type="number" min='0' onChange={handleNewPriceChange} value={editPrice}/><br/>
-              Quantity: <input type="number" min='0' onChange={handleNewQuantityChange} value={editQty}/><br/>
-              <a href="#" class="close-modal">
-                <input type="submit" value="edit product"/>
-              </a>
-            </form>
-            <a href="#" class="close-modal">Close</a>
-          </div>
+            Name: <input type="text" onChange={handlenewNameChange} value={editName}/><br/>
+            Image: <input type="text" onChange={handleNewImageChange} value={editImage}/><br/>
+            Description: <input type="text" onChange={handleNewDescriptionChange} value={editDescription}/><br/>
+            Price: <input type="number" min='0' onChange={handleNewPriceChange} value={editPrice}/><br/>
+            Quantity: <input type="number" min='0' onChange={handleNewQuantityChange} value={editQty}/><br/>
+            <input type="submit" value="edit product"/>
+          </form>
+          <a href="#" class="close-modal">Close</a>
         </div>
+      </div>
 
-        <div>
-          Search: <input type="text" onChange={(e) => {handleSearch(e.target.value)}}/>
-        </div>
+      <div id="search">
+        Search: <input type="text" onChange={(e) => {handleSearch(e.target.value)}}/>
+      </div>
 
-        {
+      {
         filtered.map((item) => {
           return(
             <div>
-            <img className="prodimg" src={item.image}/>
-            <div className="underpic">
-              <h2 className="prodName">{item.name}</h2>
-              <h3>{item.description}</h3>
-              <h2>{item.price}</h2>
-              <h2>{item.qty}</h2>
-            </div>
+              <img className="prodimg" src={item.image}/>
+              <div className="underpic">
+                <h2 className="prodName">{item.name}</h2>
+                <h3>{item.description}</h3>
+                <h2>{item.price}</h2>
+                <h2>{item.qty}</h2>
+              </div>
             </div>
           )
         })
@@ -362,30 +379,53 @@ const App = () => {
               <div>
                 <img src={products.image}/>
                 <h2>{products.name}</h2>
-                <h2>{products.sellerName}</h2>
+                <h2>Seller: {products.sellerName}</h2>
                 <h3>{products.description}</h3>
-                <h2>{products.price}</h2>
+                <h2>${products.price}</h2>
                 <h2>{products.qty}</h2>
 
-                <div>
-                {
-                  currentUser ?
-                  (currentUser.username === products.sellerName
-                  ?
-                  <div>
-                  <button onClick={ (event) => {handleDelete(products)}}>Bought!</button>
+                  {products.qty === 0 ?
+                    <h2> Out of Stock </h2> :
+                    (products.qty !== 0
+                    ?
+                    <div>
+                    <form onSubmit={handleBuyQty}>
+                      <input type="number" min='0' onChange={handleNewQuantityChange} value={newQty}/>
+                      <input type="submit" value="buy"/>
+                    </form>
+                    </div>
+                    : null)
+                  }
 
-                  <a href="#open-edit-modal">
-                  <button onClick={ (event) => {updateButton(products)}}>Edit</button>
-                  </a>
-                  </div>
-                  : null)
+                <div>
+                  {
+                    currentUser ?
+                    (currentUser.username === products.sellerName
+                    ?
+                    <div>
+                      <button onClick={ (event) => {handleDelete(products)}}>Delete</button>
+                      <a href="#open-edit-modal">
+                        <button onClick={ (event) => {updateButton(products)}}>Edit</button>
+                      </a>
+                    </div>
+                    : null)
+                  : null }
+                  { currentUser ?
+                    (currentUser.username === "admin"
+                      ?
+                      <div>
+                        <button onClick={ (event) => {handleDelete(products)}}>Delete</button>
+                        <a href="#open-edit-modal">
+                          <button onClick={ (event) => {updateButton(products)}}>Edit</button>
+                        </a>
+                      </div>
+                    : null)
                   : null}
+                  </div>
                 </div>
-              </div>
-            )
-          })
-        }
+              )
+            })
+          }
       </div>
     </main>
   );
